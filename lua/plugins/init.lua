b0d54caecfb7643af519a804f621240b5144973c
Bob Ross <55351437+bob-ross27@users.noreply.github.com>
2026-04-03 13:55:56 -0300

    update mini.nvim
    Change repo to new org.
    Use mini.icons
    
    use mini.icons over devicons
    
    remove grapple icons
    
    hardcoded dependency on devicons, remove icons instead.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,6 +3,34 @@
 --
 -- See the kickstart.nvim README for more information
 return {
+  { -- Collection of various small independent plugins/modules
+    'nvim-mini/mini.nvim',
+    config = function()
+      -- Better Around/Inside textobjects
+      --
+      -- Examples:
+      --  - va)  - [V]isually select [A]round [)]paren
+      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+      --  - ci'  - [C]hange [I]nside [']quote
+      require('mini.ai').setup { n_lines = 500 }
+
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      --
+      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+      -- - sd'   - [S]urround [D]elete [']quotes
+      -- - sr)'  - [S]urround [R]eplace [)] [']
+      require('mini.surround').setup()
+
+      -- Simple and easy statusline.
+      require('mini.statusline').setup { use_icons = true }
+
+      -- Auto pairs
+      require('mini.pairs').setup()
+
+      -- Icons
+      require('mini.icons').setup()
+    end,
+  },
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
@@ -65,9 +93,6 @@ return {
         end,
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },
-
-      -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons', enabled = true },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -495,46 +520,6 @@ return {
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
-  { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
-    config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
-
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = true }
-
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      -- statusline.section_location = function()
-      --   return '%2l:%-2v'
-      -- end
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
-
-      -- Auto pairs
-      require('mini.pairs').setup()
-    end,
-  },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
@@ -605,7 +590,7 @@ return {
   -- Edit files/directories as a buffer
   {
     'stevearc/oil.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = { 'nvim-mini/mini.nvim' },
     lazy = false,
     opts = {
       skip_confirm_for_simple_edits = true,
@@ -646,6 +631,7 @@ return {
     'cbochs/grapple.nvim',
     opts = {
       scope = 'git',
+      icons = false,
     },
     event = { 'BufReadPost', 'BufNewFile' },
     cmd = 'Grapple',
@@ -658,9 +644,6 @@ return {
       { '<M-j>', '<cmd>Grapple select index=2<cr>', desc = 'Select second tag' },
       { '<M-k>', '<cmd>Grapple select index=3<cr>', desc = 'Select third tag' },
       { '<M-l>', '<cmd>Grapple select index=4<cr>', desc = 'Select fourth tag' },
-    },
-    dependencies = {
-      'nvim-tree/nvim-web-devicons',
     },
   },
 
