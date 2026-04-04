13d5e725fe79ccfe656b9114f685294c9a9830c2
Bob Ross <55351437+bob-ross27@users.noreply.github.com>
2026-04-04 00:18:16 -0300

    use mini.pick instead of telescope

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -48,9 +48,6 @@ vim.keymap.set('v', '<M-Down>', ":m '>-2<CR>gv=gv", { desc = 'Move line down' })
 --  To jump back, press <C-t>.
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = '[G]oto [D]efinition' })
 
--- Find references for the word under your cursor.
-vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = '[G]oto [R]eferences' })
-
 -- Jump to the implementation of the word under your cursor.
 --  Useful when your language has ways of declaring types without an actual implementation.
 vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, { desc = '[G]oto [I]mplementation' })
@@ -59,14 +56,6 @@ vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, { desc = '[G]oto [I]mpleme
 --  Useful when you're not sure what type a variable is and you want to see
 --  the definition of its *type*, not where it was *defined*.
 vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, { desc = 'Type [D]efinition' })
-
--- Fuzzy find all the symbols in your current document.
---  Symbols are things like variables, functions, types, etc.
-vim.keymap.set('n', '<leader>ds', vim.lsp.buf.document_symbol, { desc = '[D]ocument [S]ymbols' })
-
--- Fuzzy find all the symbols in your current workspace.
---  Similar to document symbols, except searches over your entire project.
-vim.keymap.set('n', '<leader>ws', vim.lsp.buf.workspace_symbol, { desc = '[W]orkspace [S]ymbols' })
 
 -- Rename the variable under your cursor.
 --  Most Language Servers support renaming across files, etc.
---
+++ lua/plugins/mini.lua
@@ -0,0 +1,44 @@
+return { -- Collection of various small independent plugins/modules
+  'nvim-mini/mini.nvim',
+  config = function()
+    -- Better Around/Inside textobjects
+    --
+    -- Examples:
+    --  - va)  - [V]isually select [A]round [)]paren
+    --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+    --  - ci'  - [C]hange [I]nside [']quote
+    require('mini.ai').setup { n_lines = 500 }
+
+    -- Add/delete/replace surroundings (brackets, quotes, etc.)
+    --
+    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+    -- - sd'   - [S]urround [D]elete [']quotes
+    -- - sr)'  - [S]urround [R]eplace [)] [']
+    require('mini.surround').setup()
+
+    -- Simple and easy statusline.
+    require('mini.statusline').setup { use_icons = true }
+
+    -- Auto pairs
+    require('mini.pairs').setup()
+
+    -- Icons
+    require('mini.icons').setup()
+
+    require('mini.extra').setup()
+
+    -- Picker
+    require('mini.pick').setup()
+    vim.keymap.set('n', '<leader><leader>', '<Cmd>Pick buffers<CR>', { desc = '[ ] Find existing buffers' })
+    vim.keymap.set('n', '<leader>sh', '<Cmd>Pick help<CR>', { desc = '[S]earch [H]elp' })
+    vim.keymap.set('n', '<leader>sk', '<Cmd>Pick keymaps<CR>', { desc = '[S]earch [K]eymaps' })
+    vim.keymap.set('n', '<leader>sf', '<Cmd>Pick files<CR>', { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>sw', '<Cmd>Pick grep pattern="<cword>"<CR>', { desc = '[S]earch current [W]ord' })
+    vim.keymap.set('n', '<leader>sg', '<Cmd>Pick grep pattern=""<CR>', { desc = '[S]earch by [G]rep' })
+    vim.keymap.set('n', '<leader>sd', '<Cmd>Pick diagnostic scope="all"<CR>', { desc = '[S]earch [D]iagnostics' })
+    vim.keymap.set('n', '<leader>sr', '<Cmd>Pick resume<CR>', { desc = '[S]earch [R]esume' })
+    vim.keymap.set('n', '<leader>ds', '<Cmd>Pick lsp scope="document_symbol"<CR>', { desc = '[D]ocument [S]ymbols' })
+    vim.keymap.set('n', '<leader>ws', '<Cmd>Pick lsp scope="workspace_symbol_live"<CR>', { desc = '[W]orkspace [S]ymbols' })
+    vim.keymap.set('n', 'gr', '<Cmd>Pick lsp scope="references"<CR>', { desc = '[G]oto [R]eferences' })
+  end,
+}
